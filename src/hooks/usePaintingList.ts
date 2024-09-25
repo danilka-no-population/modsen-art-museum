3135bb51f9eac550a0cd93c99c7a40d2e0e4225a
Daniil Yazvinski <daniil.yazvinskiy@gmail.com>
2024-09-25 22:54:09 +0300

    feat: hook for debounce

--- src/hooks/usePaintingList.ts
+++ src/hooks/usePaintingList.ts
@@ -4,16 +4,16 @@ import { PaintingList } from '#types/paintings';
 import { getPaintingList } from '#utils/api';
 
 export function usePaintingList(paintingIds: number[]): PaintingList | null {
-    const throwError = useError();
+    const err = useError();
     const [paintings, setPaintings] = useState<PaintingList | null>(null);
 
     useEffect(() => {
         (() => {
             getPaintingList(paintingIds)
                 .then((paintings) => setPaintings(paintings))
-                .catch((error) => throwError(error));
+                .catch((error) => err(error));
         })();
-    }, [paintingIds, throwError]);
+    }, [paintingIds, err]);
 
     return paintings;
 }

c08227cb4249da636bdeeab7fe24e79a5cfec242
Daniil Yazvinski <daniil.yazvinskiy@gmail.com>
2024-09-25 17:28:02 +0300

    fix: fixed link for modsen logo

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { UniversalWrapper } from '#components/UniversalWrapper';
 import { Logotype } from '#components/Logotype';
 import { Link } from 'react-router-dom';
-import { ROUTES } from '#/routes';
+// import { ROUTES } from '#/routes';
 import modsenLogo from '#/assets/logotypes/modsen-logo.svg';
 import styles from './styles.module.scss';
 
@@ -9,7 +9,11 @@ export function Footer() {
     return (
         <UniversalWrapper className={styles.footer}>
             <Logotype variant="dark" />
-            <Link className={styles.logo} to={ROUTES.home.getPath()}>
+            <Link
+                className={styles.logo}
+                to={'https://www.modsen-software.com/'}
+                target="_blank"
+            >
                 <img className={styles.image} src={modsenLogo} alt="Modsen" />
             </Link>
         </UniversalWrapper>

6f604f6f734674e83f43efa77b0d87e2d399a219
Daniil Yazvinski <daniil.yazvinskiy@gmail.com>
2024-09-25 16:36:20 +0300

    feat: added footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import { UniversalWrapper } from '#components/UniversalWrapper';
+import { Logotype } from '#components/Logotype';
+import { Link } from 'react-router-dom';
+import { ROUTES } from '#/routes';
+import modsenLogo from '#/assets/logotypes/modsen-logo.svg';
+import styles from './styles.module.scss';
+
+export function Footer() {
+    return (
+        <UniversalWrapper className={styles.footer}>
+            <Logotype variant="dark" />
+            <Link className={styles.logo} to={ROUTES.home.getPath()}>
+                <img className={styles.image} src={modsenLogo} alt="Modsen" />
+            </Link>
+        </UniversalWrapper>
+    );
+}
